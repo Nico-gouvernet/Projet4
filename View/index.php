1ba292d36e0c655a2c64161e17b68cb0efacd063
Nico-gouvernet <34420570+Nico-gouvernet@users.noreply.github.com>
2018-01-08 11:04:19 +0100

    Début du Projet 4 fichier index.php
    Création du fichier Index.php

---
+++ View/index.php
@@ -0,0 +1,33 @@
+<!-- Projet 4 Création du fichier Index.php Fait par GOUVERNET Nicolas le 08/01/2018 -->
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Billet simple pour l'Alaska</title>
+        <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+        <link href="view/style.css" rel="stylesheet" type="text/css" /> 
+    </head>
+        
+    <body>
+        <h1>Billet simple pour l'Alaska</h1>
+ 
+        <?php
+        foreach($billets as $billet)
+        {
+        ?>
+        <div class="news">
+            <h3>
+                <?php echo $billet['titre']; ?>
+                <em>le <?php echo $billet['date_creation_fr']; ?></em>
+            </h3>
+            
+            <p>
+                <?php echo $billet['contenu']; ?>
+                <br />
+                <em><a href="commentaires.php?billet=<?php echo $billet['id']; ?>">Commentaires</a></em>
+            </p>
+        </div>
+        <?php
+        }
+        ?>
+    </body>
+</html>
