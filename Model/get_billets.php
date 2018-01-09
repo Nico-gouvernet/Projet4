65ab7438fd406e83ea84a9f84ccb12770c502170
Nico-gouvernet <34420570+Nico-gouvernet@users.noreply.github.com>
2018-01-09 14:42:18 +0100

    Modif de la requête $bdd  fichier get_billets

--- Model/get_billets.php
+++ Model/get_billets.php
@@ -1,14 +1,15 @@
 <!-- Création du fichier get_billets.php le 08/01/2018-->
+<!-- Modif du fichier get_billets.php le 09/01/2018-->
 <?php
-function get_billets($offset, $limit) //Mise en place d'une fonction get_billets
+function get_billets($offset, $limit)
 {
     global $bdd;
     $offset = (int) $offset;
     $limit = (int) $limit;
         
-    $req = $bdd->prepare('SELECT id, titre, contenu, DATE_FORMAT(date_creation, \'%d/%m/%Y à %Hh%imin%ss\') AS date_creation_fr FROM billets ORDER BY date_creation DESC LIMIT :offset, :limit');
-    $req->bindParam(':offset', $offset, PDO::PARAM_INT);    /* Cela indique qu'il s'agit d'un entier */
-    $req->bindParam(':limit', $limit, PDO::PARAM_INT);      /* Cela indique qu'il s'agit d'un entier */
+    $req = $bdd->prepare('SELECT id, titre, contenu, DATE_FORMAT(date_creation, \'%d/%m/%Y à %Hh%imin%ss\') AS date_creation_fr FROM billets ORDER BY date_creation LIMIT :offset, :limit');
+    $req->bindParam(':offset', $offset, PDO::PARAM_INT);    /* permet de préciser qu'il s'agit d'un entier */
+    $req->bindParam(':limit', $limit, PDO::PARAM_INT);      /* permet de préciser qu'il s'agit d'un entier */
     $req->execute();
     $billets = $req->fetchAll();
     
