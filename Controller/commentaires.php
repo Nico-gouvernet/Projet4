a19ac2a9c80fe55f8a31cc82f851be473ba8a09b
Nico-gouvernet <34420570+Nico-gouvernet@users.noreply.github.com>
2018-01-09 14:36:32 +0100

    Fichier pour les commentaires dans le (Controller)
    reprise du billet dans le dossier Model fichier get_billet.php

---
+++ Controller/commentaires.php
@@ -0,0 +1,13 @@
+<!-- Création du fichier le 09/01/2018 -->
+
+<?php
+// (**Model**)  [..Reprise du billet dans le dossier Model fichier get_billet.php ..]
+include_once('Model/get_billet.php');
+$billet = get_billet($_GET['billet']);
+
+// (**Model**)  [..Reprise des commentaires dans le dossier Model fichier get_commentaires.php ..]
+include_once('Model/get_commentaires.php');
+$commentaires = get_commentaires($_GET['billet']);
+
+// (**Model**)  [..Affichage de la vue ..]
+include_once('View/commentaires.php');
