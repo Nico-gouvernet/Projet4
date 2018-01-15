345330d6abb27556bc7df4815929b46565cc4ed5
Nico-gouvernet <34420570+Nico-gouvernet@users.noreply.github.com>
2018-01-15 17:28:54 +0100

    Modif dans le fichier commentaires.php Controller

--- Controller/commentaires.php
+++ Controller/commentaires.php
@@ -1,9 +1,13 @@
 <!-- Création du fichier le 09/01/2018 -->
+<!-- Modification du fichier le 15/01/2018 -->
 
 <?php
-// (**Model**)  [..Reprise du billet dans le dossier Model fichier get_billet.php ..]
-include_once('Model/get_billet.php');
-$billet = get_billet($_GET['billet']);
+// (**Model**)  [..Reprise du  (objet) billet dans le dossier Model fichier billet.php ..]
+include_once('Model/billet.php');
+$billet = new Billet();
+
+//recupération du billet
+$billet->getById($_GET['billet']);
 
 // (**Model**)  [..Reprise des commentaires dans le dossier Model fichier get_commentaires.php ..]
 include_once('Model/get_commentaires.php');
