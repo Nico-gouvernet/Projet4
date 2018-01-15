e383bdfc51ae89d9a0bf138272828edf2cc71f14
Nico-gouvernet <34420570+Nico-gouvernet@users.noreply.github.com>
2018-01-15 16:32:15 +0100

    Suprr pagination !! Changement d'idée dans le projet 4
    Suppression de la pagination dans le model car j'ai changé d’idée je ne veux que 5 billet dans le projet !

--- Model/pagination.php
+++
@@ -1,6 +0,0 @@
-<?php
-    //calcul du nombre de pages 
-    $requeteNbLigne = $bdd->query ('SELECT COUNT(id) as countid FROM billets');
-    $nbLigne = $requeteNbLigne->fetch();
-    $nbPage = (int)($nbLigne['countid']/5) + 1;
-?>
