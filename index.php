08a7fe2151749df7a695771c44a18953de787b5e
Nico-gouvernet <34420570+Nico-gouvernet@users.noreply.github.com>
2018-01-15 17:36:03 +0100

    Modif de l'index.php

--- index.php
+++ index.php
@@ -4,5 +4,7 @@ if (!isset($_GET['section']) OR $_GET['section'] == 'index')
 {
     include_once('Controller/index.php');
 }
-include_once('modele/connexion_sql.php');
-include_once('controleur/blog/commentaires.php');
+if (isset($_GET['section']) && $_GET['section'] == 'commentaires')
+{
+    include_once('Controller/commentaires.php');
+}
