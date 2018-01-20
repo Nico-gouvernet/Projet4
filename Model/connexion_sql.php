52bcab9a870531f73308bc91cb1a2e2437368325
Nico-gouvernet <34420570+Nico-gouvernet@users.noreply.github.com>
2018-01-20 20:00:12 +0100

    Update connexion_sql.php

--- Model/connexion_sql.php
+++ Model/connexion_sql.php
@@ -1,10 +1,11 @@
 <!-- Mise en place de la conexion pouur la $bdd  le 08/01/2018-->
-<!-- Pour le moment connexion à la $bdd avec wamp en local car le projet n'est pas mis sur le serveur de OVH !! -->
+<!-- Modif le 20/01/2018-->
+
 <?php
 // Connexion à la base de données
 try
 {
-    $bdd = new PDO('mysql:host=localhost;dbname=blog', 'root', ''); 
+    $bdd = new PDO('mysql:host=localhost;dbname=blog', 'root', '', array(PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION));
 }
 catch(Exception $e)
 {
