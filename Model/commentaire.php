df0159175fd6739d195839da61d374b4d5721e11
Nico-gouvernet <34420570+Nico-gouvernet@users.noreply.github.com>
2018-01-20 20:10:48 +0100

    Update commentaires.php

--- Model/commentaire.php
+++ Model/commentaire.php
@@ -9,7 +9,7 @@ class Commentaire
     private $_auteur;
     private $_commentaire;
     private $_date_commentaire_fr;
-    //Getters et setters
+   //Getters et setters
     public function getId() 
     {
         return $this->_id;
@@ -61,7 +61,7 @@ class Commentaire
             $this->_commentaire = $commentaire;        
         }
     }
-    public function setDate_creation_fr($date_commentaire_fr) 
+    public function setDate_commentaire_fr($date_commentaire_fr) 
     {
         if (is_string($date_commentaire_fr))        
         {        
@@ -73,7 +73,7 @@ class Commentaire
     {
         //vérification
         global $bdd;
-        if (ctype_digit($billetId)) 
+        if (is_int($billetId) || ctype_digit($billetId)) 
         {
             $billetId = (int) $billetId;
         } 
@@ -82,19 +82,71 @@ class Commentaire
             echo $billetId . ' n\'est pas un nombre.';
         }
         // éxécution de la requête
-        $req = $bdd->prepare('SELECT auteur, commentaire, DATE_FORMAT(date_commentaire, \'%d/%m/%Y à %Hh%imin%ss\') AS date_commentaire_fr FROM commentaires WHERE id_billet = :billetId ORDER BY date_commentaire');
-        $req->bindParam(':billetId', $billetId, PDO::PARAM_INT);    /* permet de préciser qu'il s'agit d'un entier */
-        $req->execute();
-        $commentaires = $req->fetchAll();
-        $req->closeCursor ();
-    
-        return $commentaires;
+        try
+        {
+            $req = $bdd->prepare('SELECT id, id_billet, auteur, commentaire, DATE_FORMAT(date_commentaire, \'%d/%m/%Y à %Hh%imin%ss\') AS date_commentaire_fr FROM commentaires WHERE id_billet = :billetId AND affichage = 2 ORDER BY date_commentaire');
+            $req->bindParam(':billetId', $billetId, PDO::PARAM_INT);    /* permet de préciser qu'il s'agit d'un entier */
+            $req->execute();
+            $commentaires = $req->fetchAll();
+            
+            $commentaires_modeles = [];
+            foreach($commentaires as $commentaire) {
+                $currentCommentaire = new Commentaire();
+                $currentCommentaire->setId($commentaire['id']);
+                $currentCommentaire->setId_billet($commentaire['id_billet']);
+                $currentCommentaire->setAuteur($commentaire['auteur']);
+                $currentCommentaire->setCommentaire($commentaire['commentaire']);
+                $currentCommentaire->setDate_commentaire_fr($commentaire['date_commentaire_fr']);
+                
+                $commentaires_modeles[] = $currentCommentaire;
+            }
+            $req->closeCursor ();
+            
+            return $commentaires_modeles;
+        }
+        catch (Exception $e)
+        {
+            echo $e->getMessage();
+            echo 'Echec de la requête vérifiez les paramètres.';
+        }   
+    }
+    public function getAllToAdmin()
+    {
+        
+        global $bdd;
+        
+        // éxécution de la requête
+        try
+        {
+            $req = $bdd->prepare('SELECT id, id_billet, auteur, commentaire, DATE_FORMAT(date_commentaire, \'%d/%m/%Y à %Hh%imin%ss\') AS date_commentaire_fr FROM commentaires WHERE affichage = 1 ORDER BY date_commentaire');
+            $req->execute();
+            $commentaires = $req->fetchAll();
+            $commentaires_modeles = [];
+            foreach($commentaires as $commentaire) {
+                $currentCommentaire = new Commentaire();
+                $currentCommentaire->setId($commentaire['id']);
+                $currentCommentaire->setId_billet($commentaire['id_billet']);
+                $currentCommentaire->setAuteur($commentaire['auteur']);
+                $currentCommentaire->setCommentaire($commentaire['commentaire']);
+                $currentCommentaire->setDate_commentaire_fr($commentaire['date_commentaire_fr']);
+                
+                $commentaires_modeles[] = $currentCommentaire;
+            }
+            $req->closeCursor ();
+        
+            return $commentaires_modeles;
+        }
+        catch (Exception $e)
+        {
+            echo $e->getMessage();
+            echo 'Echec de la requête vérifiez les paramètres.';
+        } 
     }
     public function sendCommentaire ($id_billet, $auteur, $commentaire)
     {
         global $bdd;
         //vérifications 
-        if (ctype_digit($id_billet)) 
+        if (is_int($id_billet) || ctype_digit($id_billet)) 
         {
             $id_billet = (int) $id_billet;
         } 
@@ -119,10 +171,10 @@ class Commentaire
         {
             echo $commentaire . ' n\'est pas du texte valide.';
         }
-        // éxécution de la requête
+        // éxécution de la requête, affichage a la valeur 1 = attente de modération
         try 
         {        
-            $req = $bdd->prepare ('INSERT INTO commentaires (`id`, `id_billet`, `auteur`, `commentaire`, `date_commentaire`) VALUES (NULL, :id_billet, :auteur, :commentaire, CURRENT_TIMESTAMP)');
+            $req = $bdd->prepare ('INSERT INTO commentaires (`id`, `id_billet`, `affichage` ,`auteur`, `commentaire`, `date_commentaire`) VALUES (NULL, :id_billet, 1, :auteur, :commentaire, CURRENT_TIMESTAMP)');
             $req->bindParam(':id_billet', $id_billet, PDO::PARAM_INT);
             $req->bindParam(':auteur', $auteur, PDO::PARAM_STR);
             $req->bindParam(':commentaire', $commentaire, PDO::PARAM_STR);
@@ -132,9 +184,64 @@ class Commentaire
         }
         catch (Exception $e)
         {
+            echo $e->getMessage();
             echo 'Echec de la requête vérifiez les paramètres.';
         }   
         
+    }
+    public function showCommentaireById ($id_commentaire)
+    {
+        global $bdd;
+        //vérifications 
+        if (is_int($id_commentaire) || ctype_digit($id_commentaire)) 
+        {
+            $id_commentaire = (int) $id_commentaire;
+        } 
+        else 
+        {
+            echo $id_commentaire . ' n\'est pas un nombre.';
+        }
+        // éxécution de la requête, changement de la valeur affichage à 2 pour le commentaire posédant id_commentaire
+        try 
+        {        
+            $req = $bdd->prepare ('UPDATE commentaires SET affichage = 2 WHERE id = :id_commentaire');
+            $req->bindParam(':id_commentaire', $id_commentaire, PDO::PARAM_INT);
+            $req->execute();
+            
+            $req->closeCursor ();
+        }
+        catch (Exception $e)
+        {
+            echo $e->getMessage();
+            echo 'Echec de la requête vérifiez les paramètres.';
+        }
+    }
+    public function deletCommentaireById ($id_commentaire)
+    {
+        global $bdd;
+        //vérifications 
+        if (is_int($id_commentaire) || ctype_digit($id_commentaire)) 
+        {
+            $id_commentaire = (int) $id_commentaire;
+        } 
+        else 
+        {
+            echo $id_commentaire . ' n\'est pas un nombre.';
+        }
+        // éxécution de la requête, supprimer le commentaire posédant id_commentaire
+        try 
+        {        
+            $req = $bdd->prepare ('DELETE FROM commentaires WHERE id = :id_commentaire');
+            $req->bindParam(':id_commentaire', $id_commentaire, PDO::PARAM_INT);
+            $req->execute();
+            
+            $req->closeCursor ();
+        }
+        catch (Exception $e)
+        {
+            echo $e->getMessage();
+            echo 'Echec de la requête vérifiez les paramètres.';
+        }
     }
 }
 ?>
