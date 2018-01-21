c60f281b2e1c8e4b051cb781194e2817c2621c52
Nico-gouvernet <34420570+Nico-gouvernet@users.noreply.github.com>
2018-01-21 19:51:46 +0100

    Fin du Projet 4 connexion_sql.php (Model)
    //******** [Fin du projet 4 Openclassroom] ********//
    Modification du fichier connexion_sql.php  de la structure MVC dans le dossier (Model)

--- Model/connexion_sql.php
+++ Model/connexion_sql.php
@@ -1,8 +1,4 @@
-<!-- Mise en place de la conexion pouur la $bdd  le 08/01/2018-->
-<!-- Modif le 20/01/2018-->
-
 <?php
-// Connexion à la base de données
 try
 {
     $bdd = new PDO('mysql:host=localhost;dbname=blog', 'root', '', array(PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION));
