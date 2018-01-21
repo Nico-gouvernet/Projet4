b344a532dd6a3d8c1bcc7ff220c2fbce65b92c05
Nico-gouvernet <34420570+Nico-gouvernet@users.noreply.github.com>
2018-01-21 15:49:30 +0100

    Fin du Projet 4 Head.php (View).
    //******** [Fin du projet 4 Openclassroom] ********//
    Ajout du fichier Head.php de la structure MVC dans le dossier (View)

---
+++ View/head.php
@@ -0,0 +1,4 @@
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-alpha.6/css/bootstrap.min.css" integrity="sha384-rwoIResjU2yc3z8GV/NPeZWAv56rSmLldC3R/AZzGRnGxQQKnKkoFVhFQhNUwEyJ" crossorigin="anonymous">
+<link href="View/style.css" rel="stylesheet" type="text/css" />
+<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no"> 
