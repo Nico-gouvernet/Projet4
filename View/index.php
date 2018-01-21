9917b4fdd7f5c1b061394c2b99927a12bb71cef7
Nico-gouvernet <34420570+Nico-gouvernet@users.noreply.github.com>
2018-01-21 15:28:39 +0100

    Fin du Projet 4 Index.php (View)
    //******** [Fin du projet 4 Openclassroom] ********//
    Modification du fichier Index.php de la structure MVC dans le dossier (View)

--- View/index.php
+++ View/index.php
@@ -1,36 +1,38 @@
-<!-- Projet 4 Création du fichier Index.php Fait par GOUVERNET Nicolas le 08/01/2018 -->
-<!-- Modification du fichier le 11/01/2018 Mise en place de la POO pour la pagination l'administartion et le contenu avec le billet -->
 <!DOCTYPE html>
 <html>
     <head>
         <title>Billet simple pour l'Alaska</title>
-        <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
-        <link href="View/style.css" rel="stylesheet" type="text/css" /> 
-    </head>
-        
+        <?php include_once('head.php');?>
+    </head>    
     <body>
-        <h1>Billet simple pour l'Alaska</h1>
-        
-        <!-- chargement des billets -->
+        <section id="global" class="container">
+            <header id="header" class="row">
+                <a id="mainTitle" class="col-12" href="index.php"><h1>Billet simple pour l'Alaska Jean Forteroche </h1></a>
+            </header>
+        <section id="pageContent" class="row justify-content-center"> 
+<!------------------------------------------------------------------- Chargement du billet ---------------------------------------->
         <?php
         foreach($billets as $billet)
         {
         ?>
         <div class="news">
-            <h3>
-                <?php echo $billet->getTitre(); ?>
-                <em>le <?php echo $billet->getDate_creation_fr(); ?></em>
-            </h3>
-            
-            <p>
-                <?php echo $billet->getContenu(); ?>
-                <br />
-                <em><a href="index.php?section=commentaires&billet=<?php echo $billet->getId(); ?>">Commentaires</a></em>
-            </p>
+            <h3><?php echo $billet->getTitre(); ?></h3>
+                <div><?php echo $billet->getContenu(); ?></div>
+                    <p>
+                      <em class="datePublication">Publié le <?php echo $billet->getDate_creation_fr(); ?></em>
+                      <br />
+                      <em><a href="index.php?section=commentaires&billet=<?php echo $billet->getId(); ?>" class="btn btn-secondary btn-sm">Commentaires</a></em>
+                    <p>
         </div>
         <?php
         }
-        ?>
-        <p><a href="index.php?section=log">Administration</a></p>
+        ?>  
+<!------------------------------------------------------------------- Fin du Chargement du billet ---------------------------------------->
+        </section>
+            <p>Billet simple pour l'Akaska - Jean Forteroche</p>
+            <p>Réalisé par GOUVERNET Nicolas - avec HTML5/CSS3 et PHP 7.2.1</p>
+            <p><a href="index.php?section=log" class="btn btn-secondary btn-sm" id="administration">Administration</a></p>
+        </section>    
+        <?php include_once('liensBootstrap.php');?>
     </body>
 </html>
