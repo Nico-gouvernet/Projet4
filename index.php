659fa589745d9fea90d99dcd67fbd552bb1dce1d
Nico-gouvernet <34420570+Nico-gouvernet@users.noreply.github.com>
2018-01-22 19:35:57 +0100

    Fin du Projet 4 index.php

--- index.php
+++ index.php
@@ -1,18 +1,19 @@
 <?php
+//------------------------------------------------------------------------------------------------->>>>>>>>>>>>>>>>>>>>>> [ On affiche la page connexion_sql.php ]
 include_once('Model/connexion_sql.php');
 if (!isset($_GET['section']) OR $_GET['section'] == 'index')
 {
-    include_once('Controller/index.php');
+    include_once('Controller/index.php'); //------------------------------------------------------------------------------------------------->>>>>>>>>>>>>>>>>>>>>> [ On affiche la page index.php]
 }
 if (isset($_GET['section']) && $_GET['section'] == 'commentaires')
 {
-    include_once('Controller/commentaires.php');
+    include_once('Controller/commentaires.php');//------------------------------------------------------------------------------------------------->>>>>>>>>>>>>>>>>>>>>> [ On affiche la page commentaires.php ]
 }
 if (isset($_GET['section']) && $_GET['section'] == 'log')
 {
-    include_once('Controller/log.php');
+    include_once('Controller/log.php');//------------------------------------------------------------------------------------------------->>>>>>>>>>>>>>>>>>>>>> [ On affiche la page log.php ]
 }
 if (isset($_GET['section']) && $_GET['section'] == 'admin')
 {
-    include_once('Controller/admin.php');
+    include_once('Controller/admin.php');//------------------------------------------------------------------------------------------------->>>>>>>>>>>>>>>>>>>>>> [ On affiche la page admin.php]
 }
