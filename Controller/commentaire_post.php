6e4670c132f2e655712778ffe627f90374ba509e
Nico-gouvernet <34420570+Nico-gouvernet@users.noreply.github.com>
2018-01-22 18:35:40 +0100

    Fin du Projet 4 commentaire_post.php (Controller)
    //******** [Fin du projet 4 Openclassroom] ********//
    Modification du fichier commentaire_post.php dans le dossier (Contoller)

--- Controller/commentaire_post.php
+++ Controller/commentaire_post.php
@@ -1,13 +1,23 @@
-<!-- Création du fichier le 15/01/18-->
 <?php
+//------------------------------------------------------------------------------------------------->>>>>>>>>>>>>>>>>>>>>> [ On affiche la page connexion_sql.php ]
 include_once('../../Model/connexion_sql.php');
+//------------------------------------------------------------------------------------------------->>>>>>>>>>>>>>>>>>>>>> [ On affiche la page commentaire.php ]
 include_once('../../Model/commentaire.php');
 $envoiCommentaire = new Commentaire();
-// récupération des variables nécéssaire a l'envoi
+//------------------------------------------------------------------------------------------------------------>>>>>>>>>>>>>>>>>>>[ récupération des variables nécéssaire a l'envoi]
 $id_billet = $_POST['id_billet'];
 $auteur = $_POST['auteur'];
 $commentaire = $_POST['commentaire'];
-$envoiCommentaire->sendCommentaire($id_billet, $auteur, $commentaire);
-// Redirection du visiteur vers la page des commentaires du billet
-header('Location: ../../index.php?section=commentaires&billet=' . $id_billet);
+
+if (empty($_POST['auteur']) || empty($_POST['commentaire']))
+{
+    echo"<p>Veuillez remplir tous les champs</p> <a href=../../index.php?section=commentaires&billet=$id_billet>Retour à la page</a>";
+}
+else
+{       
+    $envoiCommentaire->sendCommentaire($id_billet, $auteur, $commentaire);
+//------------------------------------------------------------------------------------------------------------>>>>>>>>>>>>>>>>>>>[Redirection du visiteur vers la page des commentaires du billet]
+    header('Refresh:5;url=../../index.php?section=commentaires&billet=' . $id_billet);
+    echo 'Commentaire validé. <br /> Vous allez revenir à la page des commentaires dans instant.';  
+}
 ?>
