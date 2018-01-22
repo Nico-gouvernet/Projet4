8e56ab31ed6f1d05ab958b8567092374ca896f63
Nico-gouvernet <34420570+Nico-gouvernet@users.noreply.github.com>
2018-01-22 17:41:16 +0100

    Fin du Projet 4 index.php (Controller)
    //******** [Fin du projet 4 Openclassroom] ********//
    Modification du fichier index.php dans le dossier (Contoller)

--- Controller/index.php
+++ Controller/index.php
@@ -1,24 +1,21 @@
-<!-- Création du fichier Index.php le 08/01/2018 -->
-<!-- Modification du fichier Index.php le 09/01/2018 -->
-<!-- Modification du fichier Index.php le 15/01/2018 -->
-
 <?php
-// (**Model**)  [.. Je prend les billets voulu dans le fichier billet.php ..]
+//------------------------------------------------------------------------------------------------->>>>>>>>>>>>>>>>>>>>>> [on inclu la class et on créé l'objet billet nécéssaire au calcul]
 include_once('Model/billet.php');
 $billet = new Billet();
 $nbPage = $billet->getPageNb();
-// On calcul la page voulu
+//------------------------------------------------------------------------------------------------->>>>>>>>>>>>>>>>>>>>>> [On calcul la page voulu]
 if(array_key_exists("page", $_GET) && isset($_GET['page']) && ctype_digit($_GET['page']))
 {
     $page = ($_GET['page'] * 5) - 5;
     $ixPage = $_GET['page'];
-    // Dépassement de la limite Max =>
+//------------------------------------------------------------------------------------------------->>>>>>>>>>>>>>>>>>>>>> [Si on a dépassé la limite max => limite max-]
+
     if($_GET['page'] > $nbPage) 
     {
         $page = ($nbPage*5) -5;
         $ixPage = $nbPage;
     }
-    // Dépassement de la limite Min =>
+ //------------------------------------------------------------------------------------------------->>>>>>>>>>>>>>>>>>>>>> [Si on a dépassé la limite min => limite min-]
     if($_GET['page'] < 1)
     {
         $page = 0;
@@ -30,15 +27,7 @@ else
     $page = 0;
     $ixPage = 1;
 }
-//on demande les billets
+//------------------------------------------------------------------------------------------------->>>>>>>>>>>>>>>>>>>>>> [on demande les billets]
 $billets = $billet->getByPage($page, 5);
-// (**Controller**)  [.. je fait du traitement sur les données ..]
-// [.. Sécurisation de l'affichage ..]
-/*
-{
-    $billets[$cle]['titre'] = htmlspecialchars($billet['titre']);
-    $billets[$cle]['contenu'] = nl2br(htmlspecialchars($billet['contenu']));
-}
-*/
-// (**View**) [.. Affichage de la page ..]
+//------------------------------------------------------------------------------------------------->>>>>>>>>>>>>>>>>>>>>> [ On affiche la page (View]
 include_once('View/index.php');
