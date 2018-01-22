8635837c882dc86d00adf7c1aae87237b901e6e5
Nico-gouvernet <34420570+Nico-gouvernet@users.noreply.github.com>
2018-01-22 18:05:44 +0100

    Fin du Projet 4 commentaires.php (Controller)
    //******** [Fin du projet 4 Openclassroom] ********//
    Modification du fichier commentaires.php  dans le dossier (Contoller)

--- Controller/commentaires.php
+++ Controller/commentaires.php
@@ -1,19 +1,15 @@
-<!-- Création du fichier le 09/01/2018 -->
-<!-- Modification du fichier 2fois le 15/01/2018 -->
-
 <?php
-// (**Model**)  [..Reprise du  (objet) billet dans le dossier Model fichier billet.php ..]
+//------------------------------------------------------------------------------------------------->>>>>>>>>>>>>>>>>>>>>> [ajout de l'objet billet]
 include_once('Model/billet.php');
 $billet = new Billet();
-
-//recupération du billet
+//---------------------------------------------------------------------------------------------------->>>>>>>>>>>>>>>>> [recupération du billet]
 $id_billet = $_GET['billet'];
 $billet->getById($id_billet);
-
-// (**Model**)  [..Reprise des commentaires dans le dossier Model fichier get_commentaires.php ..]
+//------------------------------------------------------------------------------------------->>>>>>>>>>>>>>>>> [recuperation des commentaires]
 include_once('Model/commentaire.php');
 $commentaire = new Commentaire();
 $commentaires = $commentaire->getForId($_GET['billet']);
-
-// (**View**)  [..Affichage de la vue ..]
+//----------------------------------------------------------------------------->>>>>>>>>>>>>>>>>>>>>>>[creation de l'objet pour signaler un commentaire]
+$commentaireAdmin = new Commentaire();
+//------------------------------------------------------------------------------------------------------------>>>>>>>>>>>>>>>>>>> [on affiche la View]
 include_once('View/commentaires.php');
