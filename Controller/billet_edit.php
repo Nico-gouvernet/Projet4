f0bd7e6395ed31216618be867973086fe87c91c9
Nico-gouvernet <34420570+Nico-gouvernet@users.noreply.github.com>
2018-01-23 16:14:47 +0100

    Modification ! billet_edit.php

--- Controller/billet_edit.php
+++ Controller/billet_edit.php
@@ -1,6 +1,6 @@
 <?php
 //------------------------------------------------------------------------------------------------->>>>>>>>>>>>>>>>>>>>>> [ On affiche la page connexion_sql.php ]
-include_once('../../Model/connexion_sql.php');
+include_once('../Model/connexion_sql.php');
 session_start();
 if (isset($_SESSION['encodedPassword'])) 
 {
@@ -13,12 +13,12 @@ else
 if ($est_connecte) 
 {
 //------------------------------------------------------------------------------------------------->>>>>>>>>>>>>>>>>>>>>> [ On affiche la page billet.php]
-    include_once('../../Model/billet.php');
+    include_once('../Model/billet.php');
     $editBillet = new Billet();
 //------------------------------------------------------------------------------------------------->>>>>>>>>>>>>>>>>>>>>> [récupération des variables nécéssaire a l'édition']
     $id = $_POST['modifierBillet'];
     $editBillet->getById($id);
 //------------------------------------------------------------------------------------------------->>>>>>>>>>>>>>>>>>>>>> [ On affiche la page billet_edit.php]
-    include_once("../../View/billet_edit.php");
+    include_once("../View/billet_edit.php");
 }
 ?>
