8d1785c422416493e794e3d8c535cbaa4b78958f
Nico-gouvernet <34420570+Nico-gouvernet@users.noreply.github.com>
2018-01-23 16:14:09 +0100

    Modification ! billet_delet.php

--- Controller/billet_delet.php
+++ Controller/billet_delet.php
@@ -1,12 +1,12 @@
 <?php
 //------------------------------------------------------------------------------------------------->>>>>>>>>>>>>>>>>>>>>> [ On affiche la page connexion_sql.php ]
-include_once('../../Model/connexion_sql.php');
+include_once('../Model/connexion_sql.php');
 session_start();
 //------------------------------------------------------------------------------------------------->>>>>>>>>>>>>>>>>>>>>> [ On affiche la page billet.php]
-include_once('../../Model/billet.php');
+include_once('../Model/billet.php');
 $deletBillet = new Billet();
 //------------------------------------------------------------------------------------------------->>>>>>>>>>>>>>>>>>>>>> [récupération de variable nécéssaire à la suppression]
 $id = $_POST['id'];
 $deletBillet->deletBilletById ($id);
-header('Location: ../../index.php?section=admin');
+header('Location: ../index.php?section=admin');
 ?>
