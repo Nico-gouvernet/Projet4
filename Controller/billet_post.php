7c6885051eb3906b1697f114394a3fff4ef9584d
Nico-gouvernet <34420570+Nico-gouvernet@users.noreply.github.com>
2018-01-23 16:16:08 +0100

    Modification ! billet_post.php

--- Controller/billet_post.php
+++ Controller/billet_post.php
@@ -1,13 +1,13 @@
 <?php
 //------------------------------------------------------------------------------------------------->>>>>>>>>>>>>>>>>>>>>> [ On affiche la page connexion_sql.php ]
-include_once('../../Model/connexion_sql.php');
+include_once('../Model/connexion_sql.php');
 session_start();
 //------------------------------------------------------------------------------------------------->>>>>>>>>>>>>>>>>>>>>> [ On affiche la page billet.php ]
-include_once('../../Model/billet.php');
+include_once('../Model/billet.php');
 $envoiBillet = new Billet();
 //------------------------------------------------------------------------------------------------->>>>>>>>>>>>>>>>>>>>>> [ récupération des variables nécéssaire a l'envoi ]
 $titre = $_POST['titre'];
 $contenu = $_POST['contenu'];
 $envoiBillet->sendBillet($titre, $contenu);
-header('Location: ../../index.php?section=admin');
+header('Location: ../index.php?section=admin');
 ?>
